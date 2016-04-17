d87a30dffe4deb85ce7058798e5da1b03b524e54
Slezak Attila <attila.slezak@gmail.com>
2016-04-17 18:59:02 +0100

    Exercises with Input-Output added

---
+++ SeekAndArchive/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeekAndArchive
+{
+    class Program
+    {
+        static List<FileInfo> FoundFiles;
+        static List<FileSystemWatcher> watchers;
+        static List<DirectoryInfo> archiveDirs;
+
+        static void RecursiveSearch(List<FileInfo> foundFiles, string fileName, DirectoryInfo currentDirectory)
+        {
+            foreach (FileInfo fileInf in currentDirectory.GetFiles())
+            {
+                if (fileInf.Name == fileName)
+                {
+                    foundFiles.Add(fileInf);
+                }
+            }
+            foreach (DirectoryInfo dirInf in currentDirectory.GetDirectories())
+            {
+                RecursiveSearch(foundFiles, fileName, dirInf);
+            }
+        }
+
+        static void WatcherChanged(object sender, FileSystemEventArgs e)
+        {
+            if (e.ChangeType == WatcherChangeTypes.Changed)
+            {
+                Console.WriteLine("{0} has been changed!", e.FullPath);
+
+                FileSystemWatcher senderWatcher = (FileSystemWatcher)sender;
+                int index = watchers.IndexOf(senderWatcher, 0);
+
+                ArchiveFile(archiveDirs[index], FoundFiles[index]);
+            }
+        }
+
+        static void ArchiveFile(DirectoryInfo archiveDir, FileInfo fileToArchive)
+        {
+            FileStream input = fileToArchive.OpenRead();
+            FileStream output = File.Create(archiveDir.FullName + @"\" + fileToArchive.Name + ".gz");
+
+            GZipStream Compressor = new GZipStream(output, CompressionMode.Compress);
+
+            int b = input.ReadByte();
+
+            while (b != -1)
+            {
+                Compressor.WriteByte((byte)b);
+                b = input.ReadByte();
+            }
+            Compressor.Close();
+            input.Close();
+            output.Close();
+        }
+
+        static void Main(string[] args)
+        {
+            string fileName = args[0];
+            string directoryName = args[1];
+            FoundFiles = new List<FileInfo>();
+
+            DirectoryInfo rootDir = new DirectoryInfo(directoryName);
+            if (!rootDir.Exists)
+            {
+                Console.WriteLine("The specified directory does not exist.");
+                return;
+            }
+
+            RecursiveSearch(FoundFiles, fileName, rootDir);
+
+            Console.WriteLine("Found {0} files.", FoundFiles.Count);
+
+            foreach (FileInfo filInf in FoundFiles)
+            {
+                Console.WriteLine("{0}", filInf.FullName);
+            }
+
+            watchers = new List<FileSystemWatcher>();
+
+            foreach (FileInfo filInf in FoundFiles)
+            {
+                FileSystemWatcher newWatcher = new FileSystemWatcher(filInf.DirectoryName, filInf.Name);
+                newWatcher.Changed += new FileSystemEventHandler(WatcherChanged);
+
+                newWatcher.EnableRaisingEvents = true;
+                watchers.Add(newWatcher);
+            }
+
+            archiveDirs = new List<DirectoryInfo>();
+
+            for (int i = 0; i < FoundFiles.Count; i++)
+            {
+                archiveDirs.Add(Directory.CreateDirectory("archive" + i.ToString()));
+            }
+        }
+    }
+}
