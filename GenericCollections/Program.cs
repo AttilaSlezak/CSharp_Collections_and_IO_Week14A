1c56c0e13bc3cf86e286bf83c4c9db5fd11f473d
Slezak Attila <attila.slezak@gmail.com>
2016-04-17 18:56:48 +0100

    Exercises with Collections added

---
+++ GenericCollections/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericCollections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> countryLookup = new Dictionary<int, string>();
+            countryLookup[44] = "United Kingdom";
+            countryLookup[33] = "France";
+            countryLookup[31] = "Netherlands";
+            countryLookup[55] = "Brazil";
+            
+            int codeNumber = 33;
+
+            Console.WriteLine("The {0} Code is for: {1}\n", codeNumber, countryLookup[codeNumber]);
+
+            foreach (KeyValuePair<int, string> item in countryLookup)
+            {
+                int code = item.Key;
+                string country = item.Value;
+                Console.WriteLine("Code {0} = {1}", code, country);
+            }
+        }
+    }
+}
